e9a0272c8e570068909554c87993f430608a55d3
Hristina20299 <hristina.velkova.27@gmail.com>
2020-12-05 12:50:31 +0000

    ~~

--- src/exercise10/Ellipse.java
+++ src/exercise10/Ellipse.java
@@ -1,6 +1,6 @@
 package exercise10;
 
-public class Ellipse {
+public class Ellipse extends Figure {
     private Point startPoint;
     private double a;
     private double b;
@@ -64,7 +64,16 @@ public class Ellipse {
     }
 
     public boolean containsClick(Point click) {
-        // Check if click point is inside the rectangle
-        return false;
+      
+        double clickX = click.getX();
+        double clickY = click.getY();
+
+        double centerX = startPoint.getX()+side1;
+        double centerY = startPoint.getY()+side2;
+
+         double part1 =(Math.pow(clickX - centerX, 2)) / (side1 * side1);
+         double part2 =(Math.pow(clickY - centerY, 2)) / (side2 * side2);
+
+        return part1 + part2 <= 1;   
     }
 }
